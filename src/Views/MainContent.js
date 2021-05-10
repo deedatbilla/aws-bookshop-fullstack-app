8557c8c00a8df41499004130bdfaafbc013929b0
deedatbilla <deedat5@yahoo.com>
2021-05-10 13:00:48 +0000

    misc

--- src/Views/MainContent.js
+++ src/Views/MainContent.js
@@ -1,10 +1,10 @@
 import React, { Suspense } from "react";
 import { Header } from "../Components/";
 import { Redirect, Route, Switch } from "react-router-dom";
-import BuyerRoutes from '../Routes/BuyerRoutes'
+import BuyerRoutes from "../Routes/BuyerRoutes";
 import { useAppContext } from "../Context/Context";
 import SellerRoutes from "../Routes/SellerRoutes";
-function MainContent({history}) {
+function MainContent({ history }) {
   const { user } = useAppContext();
   //   console.log(user)
   return (
@@ -13,7 +13,7 @@ function MainContent({history}) {
       <Suspense fallback={<div>loading</div>}>
         {user?.attributes ? (
           <>
-            {user.attributes["custom:role"]==="seller" ? (
+            {user.attributes["custom:role"] === "seller" ? (
               <Switch>
                 {SellerRoutes.map((route, idx) => {
                   return (
@@ -30,22 +30,24 @@ function MainContent({history}) {
                 })}
                 <Redirect from="/" to="/dashboard" />
               </Switch>
-            ) : <Switch>
-            {BuyerRoutes.map((route, idx) => {
-              return (
-                route.component && (
-                  <Route
-                    key={idx}
-                    path={route.path}
-                    exact={route.exact}
-                    name={route.name}
-                    render={(props) => <route.component {...props} />}
-                  />
-                )
-              );
-            })}
-            <Redirect from="/" to="/books" />
-          </Switch>}
+            ) : (
+              <Switch>
+                {BuyerRoutes.map((route, idx) => {
+                  return (
+                    route.component && (
+                      <Route
+                        key={idx}
+                        path={route.path}
+                        exact={route.exact}
+                        name={route.name}
+                        render={(props) => <route.component {...props} />}
+                      />
+                    )
+                  );
+                })}
+                <Redirect from="/" to="/books" />
+              </Switch>
+            )}
           </>
         ) : null}
       </Suspense>
